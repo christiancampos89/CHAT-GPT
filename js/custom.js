1898cf953514d43c5043c5abb15732d80e8b2897
CHRISTIAN RAFAEL DE CAMPOS <95776231+christiancampos89@users.noreply.github.com>
2023-06-16 10:34:26 -0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,101 @@
+// Receber o SELECTOR do formulário
+const formPerguntaChat = document.getElementById('form-pergunta-chat');
+
+// Chave da API do OPENAI
+const OPENAI_API_KEY = "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";
+
+// Verificar se tem a chave
+if(OPENAI_API_KEY === ""){
+    document.getElementById('pergunta').innerHTML = "<span style='color: #f00;'>Necessário colocar a chave na API no arquivo custom.js</span>";
+}
+
+// Acessa o IF quando tem o SELETOR na página HTML
+if (formPerguntaChat) {
+
+    // Aguardar o usuário clicar no botão Enviar
+    formPerguntaChat.addEventListener("submit", async (e) => {
+
+        // Bloquear o recarregamento da página
+        e.preventDefault();
+
+        // Substituir o texto do botão para "Pesquisando..."
+        document.getElementById('btn-pergunta-chat').value = "Pesquisando...";
+
+        // Receber o valor do campo pergunta
+        let pergunta = document.getElementById('campo-pergunta').value;
+        //console.log(pergunta);
+
+        // Enviar o texto da pergunta para a página HTML
+        //document.getElementById('pergunta').innerHTML = pergunta;
+        
+        // Limpar a resposta
+        document.getElementById('resposta').innerHTML = "<span></span>";
+
+        // Requisição para chatgpt
+        await fetch("https://api.openai.com/v1/completions", {
+
+            // Método para enviar os dados
+            method: "POST",
+
+            // Dados ennviados no cabeçalho da requisição
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + OPENAI_API_KEY,
+            },
+
+            // Enviar os dados no corpo da requisição
+            body: JSON.stringify({
+                model: "text-davinci-003", //Modelo
+                prompt: pergunta, // Texto da pergunta
+                max_tokens: 2048, // Tamanho da resposta
+                temperature: 0 // Criatividade na resposta
+            }),
+        })
+            // Acessa o then quando obtiver resposta
+            .then((resposta) => resposta.json())
+            .then((dados) => {
+                //console.log(dados);
+                //console.log(dados.choices[0].text);
+
+                // Enviar o texto da resposta para a página HTML
+                document.getElementById('resposta').innerHTML = dados.choices[0].text;
+                document.getElementById('btn-resposta').value = dados.choices[0].text;
+
+                
+            })
+            // Retorna catch quando gerar erro
+            .catch(() => {
+                // Enviar o texto da resposta para a página HTML
+                document.getElementById('resposta').innerHTML = "Sem resposta";
+            });
+
+        // Substituir o texto do botão para "Enviar"
+        document.getElementById('btn-pergunta-chat').value = "Enviar";
+
+        // Limpar a resposta
+        document.getElementById('resposta').innerHTML = "<span></span>";
+
+        
+        var pergunta_chat = document.getElementById('campo-pergunta').value;
+        var resposta_chat = document.getElementById('btn-resposta').value;
+        //console.log(pergunta_chat);
+        //console.log(resposta_chat);
+
+        // Criar o Array de objetos
+        let historico = new Array();
+
+        // Verifica se já existe esse Array criado no LocalStorage, caso exista ele pega os dados de lá, converte e adiciona a histórico
+        if(localStorage.hasOwnProperty("historico")){
+            historico = JSON.parse(localStorage.getItem("historico"));
+        }
+
+        //Adiciona no final da fila a nova pergunta e a nova resposta
+        historico.push({pergunta_chat, resposta_chat});
+
+        //Seta as perguntas e respostas no localStorage
+        localStorage.setItem("historico", JSON.stringify(historico));
+        
+        document.getElementById("conteudo").insertAdjacentHTML('beforeend', "Pergunta: " + pergunta_chat + "<br><br>Resposta: " + resposta_chat + "<br><hr>");
+    });
+}
